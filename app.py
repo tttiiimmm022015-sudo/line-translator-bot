bf0aebc5d247b216e848726ac49751da601bd569
tttiiimmm022015-sudo <tttiiimmm022015@gmail.com>
2026-07-25 20:35:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, abort
+
+from linebot.v3.exceptions import InvalidSignatureError
 
 from line_handler import handler
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "LINE Translator Bot is running!"
 
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers.get("X-Line-Signature")
+    signature = request.headers.get(
+        "X-Line-Signature",
+        "",
+    )
 
-    body = request.get_data(as_text=True)
+    body = request.get_data(
+        as_text=True,
+    )
 
-    handler.handle(body, signature)
+    try:
+        handler.handle(body, signature)
+
+    except InvalidSignatureError:
+        abort(400)
 
     return "OK"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+    )
